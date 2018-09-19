7923610dacda1a338fd11b5bd01ee10dc4cabcca
xiaoqi7777 <501798525@qq.com>
2018-09-19 22:42:56 +0800

    1

--- src/components/component/indexList.vue
+++ src/components/component/indexList.vue
@@ -42,8 +42,8 @@ export default {
    mounted(){
     console.log('进来了')
     this.axio.get('/').then(data=>{
-      console.log('11',data.data)
       let res = data.data
+      console.log('signature',res.signature)
       wx.config({
           debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: 'wx3df629936bf31f75', // 必填，公众号的唯一标识
