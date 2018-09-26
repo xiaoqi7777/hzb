cedc3567e9533c43e98184e5c22f5f3a6d41afe2
宋格 <1253032175@qq.com>
2018-09-26 15:14:03 +0800

    1

--- src/components/component/indexList.vue
+++ src/components/component/indexList.vue
@@ -75,6 +75,13 @@ export default {
       id:null,
     };
   },
+  created(){
+    that.axio.get(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx3df629936bf31f75&redirect_uri=http://tsml520.cn/wx/he_live&response_type=code&scope=SCOPE&state=STATE#wechat_redirect`)
+      .then(data=>{
+        console.log('123')
+      })
+    
+  },
   methods:{
     btn5(){
       let that = this
